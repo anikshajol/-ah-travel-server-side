23f4cfc050b3e64eb4fb63133667055df500c7bb
anikshajol <anikshajol@gmail.com>
2022-11-09 16:42:07 +0600

    environment variable setup

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express());
 
-const uri =
-  "mongodb+srv://<username>:<password>@cluster0.d1298ft.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:process.${process.env.DB_PASSWORD}@cluster0.d1298ft.mongodb.net/?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
