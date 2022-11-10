dd9000851f2fd50712bfd39011d4df6f1a8a4695
anikshajol <anikshajol@gmail.com>
2022-11-10 19:11:45 +0600

    review collection added

--- index.js
+++ index.js
@@ -1,25 +1,53 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 //middle wares;
 app.use(cors());
-app.use(express());
+app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:process.${process.env.DB_PASSWORD}@cluster0.d1298ft.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.d1298ft.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run() {
+  try {
+    const serviceCollection = client.db("ahTravels").collection("services");
+    const reviewCollection = client.db("ahTravels").collection("review");
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+
+      res.send(services);
+    });
+
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+      console.log(service);
+    });
+
+    // review api
+
+    app.post("/review", async (req, res) => {
+      const review = req.body;
+      const result = await reviewCollection.insertOne(review);
+      res.send(result);
+    });
+  } finally {
+  }
+}
+
+run().catch((err) => console.error(err));
 
 app.get("/", (req, res) => {
   res.send("Ah-travels server is running");
